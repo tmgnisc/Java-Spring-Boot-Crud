174a7623ec161dac8fb0ec5da143c3ae9e0dc2f9
SHIFU <tamangnischal2018@gmail.com>
2024-03-06 16:14:16 +0545

    application properties config

--- src/main/java/com/Learning/model/Product.java
+++ src/main/java/com/Learning/model/Product.java
@@ -1,7 +1,18 @@
 package com.Learning.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT_TBL")
 public class Product {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int quantity;
