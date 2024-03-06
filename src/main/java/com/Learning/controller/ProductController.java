c0b31b030691dd4d89a01cd91ef2b561cf157320
SHIFU <tamangnischal2018@gmail.com>
2024-03-06 16:54:12 +0545

    Get API  ProductController

--- src/main/java/com/Learning/controller/ProductController.java
+++ src/main/java/com/Learning/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.Learning.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,26 @@ public class ProductController {
 
 	@Autowired
 	private ProductService service;
+	
+	@PostMapping("/addProduct")
 	public Product addProduct(@RequestBody Product product) {
 		return service.saveProduct(product);
 	}
 	
+	@PostMapping("/addProducts")
 	public List<Product> addProducts(@RequestBody List<Product> products){
 		return service.saveProducts(products);
+	}
+	
+	public List<Product> findAllProducts(){
+		return service.getProducts();
+	}
+	
+	public Product findProductById(int id) {
+		return service.getProductByID(id);
+	}
+	
+	public Product findProductByName(String name) {
+		return service.getProductByName(name);
 	}
 }
