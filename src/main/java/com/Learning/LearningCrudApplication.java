d7e78bc9c8410ff7a4a4a0bee98d480882620865
SHIFU <tamangnischal2018@gmail.com>
2024-03-06 16:07:25 +0545

    first commit

---
+++ src/main/java/com/Learning/LearningCrudApplication.java
@@ -0,0 +1,13 @@
+package com.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Learning/LearningCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
